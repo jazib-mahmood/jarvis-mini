b71f548071f7340e04e1584dbd80cc889a5ad9ee
jazib <jazib.mehmood@clickchain.com>
2017-12-09 14:21:24 +0500

    added test files

---
+++ test/tests.js
@@ -0,0 +1,12 @@
+var expect  = require('chai').expect;
+var responseEvaluator = require('./../response-module/responses.js');
+
+it('Main page content', function(done) {
+   expect (responseEvaluator.getResponse('hi')).to.equal('hey')
+    done();
+});
+
+it('Main page content', function(done) {
+    expect (responseEvaluator.getResponse('Whatsup')).to.equal('idk what you saying man')
+     done();
+ });
